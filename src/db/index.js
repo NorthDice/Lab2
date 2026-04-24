89d06e6c1e52e8fff5b97392bd0ecacfeb90a769
NorthDice <mister.balychev@gmail.com>
2026-04-24 21:14:22 +0100

    lab 3

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,12 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Client } from 'pg';
+import pkg from 'pg';
 import * as schema from './schema.js';
+
+const { Client } = pkg;
 
 const client = new Client({
   host: '127.0.0.1',
-  port: 5432,
+  port: 5433,
   user: 'nodejs_course_admin',
   password: 'my_password',
   database: 'nodejs_course_database',
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import productRoutes from './product.routes.js';
+import userRoutes from './user/user.router.js';
 import { logRequest } from './middleware.js';
 import { errorResponder } from './error.middleware.js';
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 app.use(logRequest);
 app.use(productRoutes);
+app.use(userRoutes);
 app.use(errorResponder);
 
 app.listen(PORT, () => {
